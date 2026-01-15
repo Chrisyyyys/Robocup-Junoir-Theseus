b5ba5028726eae6596cf4ff359d719ccbaafaf53
Chrisyyyys <wangchen.shuu@gmail.com>
2026-01-14 21:21:23 -0500

    fix maze tile class
    fixe mazetile class

--- Main/Main.ino
+++ Main/Main.ino
@@ -8,6 +8,7 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include "PID.h"
+#include "MazeTile.h"
 // set up mux and distance senosrs
 VL53L0X sensors[7];
 QWIICMUX myMux;
@@ -41,6 +42,8 @@ const double gear_ratio = 195;
 const double wheel_diameter = 68.7; // millimeters.
 // detection classes
 char classes[6] = {'H','S','U','R','Y','G'};
+// map size variables
+const int MAP_SIZE=20;
 
 // when something is seen, it would wait for 5 detections.
 void setup(){
@@ -77,4 +80,21 @@ void loop(){
   delay(1000);
   encoderCountB = 0;
   */
+}
+void initializeMap() {
+    for (int x = 0; x < MAP_SIZE; x++) {
+        for (int y = 0; y < MAP_SIZE; y++) {
+
+            mapGrid[x][y].discovered = false;
+            mapGrid[x][y].fullyExplored = false;
+
+            for (int d = 0; d < 4; d++) {
+                mapGrid[x][y].wall[d] = false;
+                mapGrid[x][y].edge[d] = false;
+            }
+
+            mapGrid[x][y].tileType = false;
+            mapGrid[x][y].victim = false;
+        }
+    }
 }
--- Main/MazeTile.cpp
+++ Main/MazeTile.cpp
@@ -1,26 +1,19 @@
-#include "bitset"
+#include "MazeTile.h"
 
-// Directions: 0 = NORTH, 1 = EAST, 2 = SOUTH, 3 = WEST
-struct Tile {
-    bool discovered;        // robot has visited this tile at least once
-    bool fullyExplored;     // no remaining unexplored edges
+// Constructor definition
+Tile::Tile() {
+    discovered = false;
+    fullyExplored = false;
+    tileType = BLANK;
+    victim = false;
 
-    bool wall[4];           // true if wall in that direction
-    bool edge[4];           // true if robot has traveled through that edge
-
-    int tiletype;           // accounts for tiletypes (0 for normal, 1 for blue, 2 for black (later can adapt to other tiletypes if needed))
-    bool victim;            // true if victim detected here
-
-    // Constructor to initialize everything to false
-    Tile() {
-        discovered = false;
-        fullyExplored = false;
-        tileType = 0;
-        victim = false;
-        
-        wall[4] = {false}; //initializes all to false. no need to loop, saves processing time.
-        edge[4] = {false}; 
+    for (int i = 0; i < 4; i++) {
+        wall[i] = false;
+        edge[i] = false;
     }
-};
+}
 
-
+// Helper definition
+Direction opposite(Direction d) {
+    return (Direction)((d + 2) % 4);
+}
