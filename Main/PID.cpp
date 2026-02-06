fc85dd2a432b6ba0f191ae6171187bfbe3bd82e8
Chrisyyyys <wangchen.shuu@gmail.com>
2026-02-06 11:06:20 -0500

    Fix navigation, motion control, and sensor handling bugs

--- Main/PID.cpp
+++ Main/PID.cpp
@@ -7,12 +7,16 @@ PID::PID(double _kp, double _ki, double _kd){
   ki = _ki;
   kd = _kd;
   previousTime = micros();
+  prevError = 0;
+  cumError = 0;
 
 }
 double PID::getPID(double _error){
   error = _error;
   currentTime = micros(); // functions are slower! you need to use micros
-  delta = (error-prevError)/(currentTime - previousTime);
+  unsigned long dt = currentTime - previousTime;
+  if (dt == 0) dt = 1;
+  delta = (error-prevError)/dt;
   cumError += error;
   double output = kp*error + ki*cumError + kd*delta;
   previousTime = currentTime;
